baf2c287b0ea26cd0de3b2045b6d5f70d3912801
mbakin <mehmetberkakinn@outlook.com>
2021-11-10 22:09:20 +0300

    create routes folder

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const { router } = require('./routes/posts');
 dotenv.config();
+
+// Middleware
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
 app.use(cors());
 
+app.use('/posts',router);  
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
