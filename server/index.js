b987edf116ffeee5433b17819dce8de648db2932
mbakin <mehmetberkakinn@outlook.com>
2021-11-10 21:10:32 +0300

    connection DB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import mongoose from "mongoose";
+import  mongoose from "mongoose";
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://memories-app:memories-app123@cluster0.5t5vn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error.message));
+
