83e7cc57978bad509828014f13273d05c1a39461
mbakin <mehmetberkakinn@outlook.com>
2021-11-10 21:50:40 +0300

    fix: leak variables

--- server/index.js
+++ server/index.js
@@ -1,24 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import  mongoose from "mongoose";
-
-
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-
-const CONNECTION_URL = 'mongodb+srv://memories-app:memories-app123@cluster0.5t5vn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.log(error.message));
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() =>  app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
+  .catch(err => console.log(err));
 
