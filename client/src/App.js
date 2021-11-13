e5cf6d6ad8d58884bc833afcda01f78feee70283
mbakin <mehmetberkakinn@outlook.com>
2021-11-13 21:56:46 +0300

    create JSX structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Container, AppBar , Typography, Grid, Grow } from "@material-ui/core";
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import memories from "./assets/memories.png";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center" color="inherit">Memories</Typography>
+        <img src={memories} alt="memories" height="600"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} md={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Post from './Post/Post'
+
+const Posts = () => {
+  return (
+    <>
+    <h1> Posts Components</h1>
+
+    <Post/>
+    <Post/>
+    </>
+  )
+}
+
+export default Posts
