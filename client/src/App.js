08a982366f9536a12a88d41ac8ea37c4070f2027
mbakin <mehmetberkakinn@outlook.com>
2021-11-21 13:22:10 +0300

    create action creators

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar , Typography, Grid, Grow } from "@material-ui/core";
+import { useDispatch} from "react-redux";
+
+import {getPosts} from "./actions/posts";
+
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import memories from "./assets/memories.png";
@@ -8,7 +12,11 @@ import useStyles from './styles'
 const App = () => {
 
   const classes = useStyles();
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
 
   return (
     <Container maxWidth="lg">
