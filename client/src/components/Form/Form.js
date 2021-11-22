1468d37358d1321e7d035ca02abdfee431ed7f06
mbakin <mehmetberkakinn@outlook.com>
2021-11-22 14:36:45 +0300

    create Form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
+import FileBase from 'react-file-base64'
 import useStyles from './styles'
 
 
 const Form = () => {
-const classes = useStyles();
+
+  const [postData, setPostData] = useState({
+    title: '',
+    creator: '',
+    message: '',
+    tags: '',
+    selectedFile: ''
+  })
+  const classes = useStyles();
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleClear = () => {
+
+ setPostData({
+      title: '',
+      creator: '',
+      message: '',
+      tags: '',
+      selectedFile: ''
+    })
+  }
+
+
   return (
     <h1>
-      Form Components
+      <Paper className={classes.paper}>
+        <form autoComplete="false" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+          <Typography variant="h6">Creator a Memory</Typography>
+          <TextField name="creator" label="Creator" variant="outlined" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+          <TextField name="title" label="Title" variant="outlined" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+          <TextField name="message" label="Message" variant="outlined" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+          <TextField name="tags" label="Tags" variant="outlined" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
+          <div className={classes.fileInput}>
+            <FileBase type="file" multiple={false} onDone={ ({base64}) => setPostData({ ...postData, selectedFile: base64 }) } />
+          </div>
+          <Button variant="contained" size="large" color="primary" type="submit" className={classes.buttonSubmit} fullWidth>Submit</Button>
+          <Button variant="contained" size="small" color="secondary" type="submit" onClick={handleClear} fullWidth>Clear</Button>
+        </form>
+      </Paper>
     </h1>
   )
 }
--- server/index.js
+++ server/index.js
@@ -20,5 +20,11 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() =>  app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
   .catch(err => console.log(err));
-  console.log(`:__: Connected DB successfully :__:`) 
 
+console.log(`
+  '-_-' '-_-''-_-''-_-''-_-''-_-' '-_-'
+  '-_-'                           '-_-'
+  '-_-' Connected DB successfully '-_-'
+  '-_-'                           '-_-'
+  '-_-' '-_-''-_-''-_-''-_-''-_-' '-_-'
+`)
