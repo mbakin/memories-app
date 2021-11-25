07cd773172b4598ac916d400f28a265ea78383ee
mbakin <mehmetberkakinn@outlook.com>
2021-11-25 12:05:11 +0300

    add a updatePost route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { getPosts, createPost } = require('../controllers/posts');
+const { getPosts, createPost, updatePost } = require('../controllers/posts');
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 module.exports = { router };
