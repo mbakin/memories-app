3e2e9e1f9d831bfaeeba253716c592674d128ca3
mbakin <mehmetberkakinn@outlook.com>
2021-11-27 17:17:27 +0300

    create 'Like' <Button>  functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 import useStyles from './styles'
 import { useDispatch } from 'react-redux'
 
-import { deletePost } from '../../../actions/posts'
+import { deletePost, likePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
@@ -36,9 +36,9 @@ const Post = ({ post, setCurrentId }) => {
         <Typography variant="h5" gutterBottom>{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.actions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize="small"/>
-          Like
+          Like {` `}
           {post.likeCount}
         </Button>
         <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
