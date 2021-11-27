a56a81c01c1891cb1bdb326e278fffaec057ac63
mbakin <mehmetberkakinn@outlook.com>
2021-11-27 13:21:04 +0300

    create 'Delete' button functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,11 +5,13 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles'
+import { useDispatch } from 'react-redux'
 
+import { deletePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
-const classes = useStyles();
-
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <Card className={classes.card}>
@@ -39,7 +41,7 @@ const classes = useStyles();
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small"/>
           Delete
         </Button>
