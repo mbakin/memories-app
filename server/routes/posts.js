b4e2a47379730192c696947aa83a6f5de8a68172
mbakin <mehmetberkakinn@outlook.com>
2021-11-27 13:16:16 +0300

    create `deletePost` router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
-const { getPosts, createPost, updatePost } = require('../controllers/posts');
+const { getPosts, createPost, updatePost, deletePost } = require('../controllers/posts');
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 module.exports = { router };
